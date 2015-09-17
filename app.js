e5e5b4ec2894901c881d6081f7e748b94e7bd210
typicalmike002 <townsendm105@gmail.com>
2015-09-17 19:11:11 -0400

    removed browserify_middleware

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var bodyParser = require('body-parser');
-var browserify = require('browserify-middleware');
 
 
 var routes = require('./routes/index');
